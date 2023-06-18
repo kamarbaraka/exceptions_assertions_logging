4119eb5b6f64f766ee04d9d78a91976d44294d09
kamarbaraka <kamar254baraka@gmail.com>
2023-06-18 18:18:33 +0300

    using logging
    Signed-off-by: Kb_hacks <kamar254baraka@gmail.com>

--- src/main/java/assertions/AssertionTest.java
+++ src/main/java/assertions/AssertionTest.java
@@ -1,6 +1,10 @@
 package assertions;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
 
 /**
  * a program to demonstrate the proper use of java assertions
@@ -8,14 +12,27 @@ import java.util.ArrayList;
  * @since 18/06/2023*/
 public class AssertionTest {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        //set the logging configuration
+        var file = "src/main/resources/logging.properties";
+        System.setProperty("java.util.logging.config.file", file);
+        LogManager.getLogManager().readConfiguration();
+
+        var logger = Logger.getGlobal();
+        logger.entering("assertions.AssertionTest", "main", args);
+        logger.severe("the first log");
+
         var cl = ClassLoader.getPlatformClassLoader();
         cl.setDefaultAssertionStatus(true);
+        logger.warning("hello, warning");
 
+        logger.fine("its fine");
         var intArray = new double[] {25, 8, 4, -16, -24, 144};
         var ass = new Assertion(intArray);
+        logger.info("just info");
 
         System.out.println(ass.sqrt());
+        logger.exiting("assertions.AssertionTest", "main");
     }
 }
 
@@ -31,6 +48,8 @@ class Assertion{
     }
 
     public ArrayList<Double> sqrt(){
+        var logger = Logger.getLogger("assertion.Assertion");
+        logger.entering("assertion.Assertion", "sqrt", this.intArray);
         var arayList = new ArrayList<Double>();
 
         for (double i : this.intArray) {
@@ -40,6 +59,7 @@ class Assertion{
         arayList.forEach(each -> each = Math.sqrt(each)); //must be positive
         System.out.println(arayList);
 
+        logger.exiting("assertion.Assertion", "sqrt", arayList);
         return arayList;
     }
 }
